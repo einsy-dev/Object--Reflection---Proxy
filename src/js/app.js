6aeb10ea6e82cfa3628599012cc54ed288798053
“einsy-dev” <116911954+einsy-dev@users.noreply.github.com>
2023-06-05 22:12:39 +0600

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,10 @@
 export default function orderByProps(obj, array) {
+  const result = [];
+  for (const key in obj) {
+    result.push({ key: key, value: obj[key] });
+  };
+  result.sort((a, b) => a.key > b.key ? 1 : -1);
+  result.sort((a) => array.includes(a.key) ? -1 : 1);
 
-    let result = [];
-
-    for (const key in obj) {
-        result.push({ key: key, value: obj[key] });
-    }
-
-    result.sort((a, b) => a.key > b.key ? 1 : -1);
-    result.sort((a) => array.includes(a.key) ? -1 : 1);
-
-    return result;
-}
+  return result;
+};
